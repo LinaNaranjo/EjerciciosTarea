93a58adf8372c32adbad9143410c546dd84d8993
Lina Naranjo <lina.naja1807@gmail.com>
2023-12-18 19:19:20 -0500

    ULTIMA MODIFICACION

---
+++ Index6.js
@@ -0,0 +1,26 @@
+// Considerando que �  =  4, � = 7, � = 2�, � = � , � = 3�, 𝑢 = 6, � = 10; crear un algoritmo con JavaScript que realice y muestre el resultado en consola de las siguientes operaciones algebraica:
+
+const a = 4;
+const b = 7;
+const c = 2*b;
+const x = a;
+const y = 3*x;
+const u = 6;
+const w = 10;
+const z = 8;
+
+// Ejercicio a
+const ejercicioa = a / (b*c);
+console.log("El resultado es: "+ejercicioa);
+
+//Ejercicio b
+const ejerciciob = Math.pow(a,2) + Math.pow(b,92);
+console.log("El resultado es: "+ejerciciob);
+
+//Ejercicio c
+const ejercicioc = (x+y) / (u+(w/a));
+console.log("El resultado es: "+ejercicioc);
+
+//Ejercicio d
+const ejerciciod = (x/y) * (z+w);
+console.log("El resultado es: "+ejerciciod);
---
+++ Index7.js
@@ -0,0 +1,22 @@
+//Ahora tu trabajo será completar las siguientes tablas de verdad, resolviendo cada
+//expresión  lógica  desde  un  algoritmo  usando  JavaScript  y  modificando  los diferentes valores que toma las variables de proposición p y q:
+
+
+// Ejercicio a fila 1 
+let p = true;
+let q = true;
+const resultado1 = !p && q;
+console.log("Es: "+resultado1);
+
+// Ejercicio a fila 2 
+let p = true;
+let q = true;
+const resultado1 = !p && q;
+console.log("Es: "+resultado1);
+
+// Ejercicio b fila 1
+let p1 = true;
+let q1 = true;
+const resultado2 = !p && !q;
+console.log("Es: "+resultado2);
+
