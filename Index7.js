1a2ce4085509361bb50b39a6aa520f46271b4ba9
Lina Naranjo <lina.naja1807@gmail.com>
2023-12-19 20:51:22 -0500

    Trabajo completo

--- Index7.js
+++ Index7.js
@@ -5,18 +5,47 @@
 // Ejercicio a fila 1 
 let p = true;
 let q = true;
-const resultado1 = !p && q;
-console.log("Es: "+resultado1);
+const resultado = !p && q;
+console.log("Es: "+resultado);
 
 // Ejercicio a fila 2 
-let p = true;
-let q = true;
-const resultado1 = !p && q;
+let p1 = true;
+let q1 = false;
+const resultado1 = !p1 && q1;
 console.log("Es: "+resultado1);
 
-// Ejercicio b fila 1
-let p1 = true;
-let q1 = true;
-const resultado2 = !p && !q;
+// Ejercicio a fila 3
+let p2 = false;
+let q2 = true;
+const resultado2 = !p2 && q2;
 console.log("Es: "+resultado2);
 
+// Ejercicio a fila 4
+let p3 = false;
+let q3 = false;
+const resultado3 = !p3 && q3;
+console.log("Es: "+resultado3);
+
+// Ejercicio b fila 1
+let p1a = true;
+let q1a = true;
+const resultado1a = !p1a && !q1a;
+console.log("Es: "+resultado1a);
+
+// Ejercicio b fila 2
+let p2a = true;
+let q2a = false;
+const resultado2a = !p2a && !q2a;
+console.log("Es: "+resultado2a);
+
+// Ejercicio b fila 3
+let p3a = false;
+let q3a = true;
+const resultado3a = !p3a && !q3a;
+console.log("Es: "+resultado3a);
+
+// Ejercicio b fila 4
+let p4a = false;
+let q4a = false;
+const resultado4a = !p4a && !q4a;
+console.log("Es: "+resultado4a);
